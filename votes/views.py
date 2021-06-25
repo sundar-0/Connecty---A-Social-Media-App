ec9c9372cf737fc742e2e05d8b58d41828342882
Sundar Dumre <72253517+sundar-0@users.noreply.github.com>
2021-06-25 11:58:14 +0545

    Update views.py

--- votes/views.py
+++ votes/views.py
@@ -1,43 +1,32 @@
-from .models import Vote
-from django.shortcuts import render
-from rest_framework import permissions, status, viewsets
-from . serializers import VoteSerializer
 from posts.models import Post
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from . permissions import hasSelfVotedOrReadOnly
+from votes.permissions import hasSelfVotedOrReadOnly
+from django.shortcuts import get_object_or_404, render
+from rest_framework import serializers, viewsets,status,permissions
+from . models import Vote
+from . serializers import VoteSerializer
+
+
 # Create your views here.
 class VoteViewSet(viewsets.ModelViewSet):
-    """
-    Votes
-    """
-    queryset = Vote.objects.all()
-    serializer_class = VoteSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,hasSelfVotedOrReadOnly]
+    queryset=Vote.objects.all()
+    serializer_class=VoteSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly,hasSelfVotedOrReadOnly]
     def perform_create(self, serializer):
-        post_instance= get_object_or_404(Post, pk=self.request.data['post'])
+        post_instance=get_object_or_404(Post,pk=self.request.data['post'])
+
+        #if user likes the post
         if self.request.data['up_vote']:
-            print(Vote.objects.filter(post=post_instance,up_vote_by=self.request.user).exists())
-
-            if Vote.objects.filter(post=post_instance,up_vote_by=self.request.user).exists():
-                return Response({"message":"You have already liked the post"})
+            already_up_voted=Vote.objects.filter(post=post_instance,up_vote_by=self.request.user).exists()
+            if already_up_voted:
+                raise serializers.ValidationError({"message":"You have already liked this post"})
             else:
-                serializer.save(up_vote_by=self.request.user,post=post_instance)  
-                
-        elif self.request.data['down_vote']:
-
-            if Vote.objects.filter(post=post_instance,down_vote_by=self.request.user).exists():
-                return Response({"message":"You have already liked the post"})
-            else:    
+                serializer.save(up_vote_by=self.request.user,post=post_instance)
+        #if dislikes
+        else:
+            already_down_voted=Vote.objects.filter(post=post_instance,down_vote_by=self.request.user).exists()
+            if already_down_voted:
+                raise serializers.ValidationError({"message":"You have already disliked this post"})
+            else:
                 serializer.save(down_vote_by=self.request.user,post=post_instance)
-           
-           
         
-        
-
-  
-  
-  
-  
-
 
